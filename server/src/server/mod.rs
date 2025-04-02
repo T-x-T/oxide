cdf05665667750dc7d632af26bb76af29c23267a
T-x-T <37040316+T-x-T@users.noreply.github.com>
2025-04-02 20:21:44 +0200

    do the multithreading in the server for handling connections so multiple players can connect at the same time

--- server/src/server/mod.rs
+++ server/src/server/mod.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::net::{TcpListener, TcpStream, SocketAddr};
+use std::sync::{Arc, Mutex};
 use lib::ConnectionState;
 
 pub mod packet;
@@ -7,25 +8,23 @@ pub mod packet;
 pub fn initialize_server() {
   let listener = TcpListener::bind("127.0.0.1:25565").unwrap();
   
-  let mut connection_states: HashMap<SocketAddr, ConnectionState> = HashMap::new();
+  let mut connection_states: Arc<Mutex<HashMap<SocketAddr, ConnectionState>>> = Arc::new(Mutex::new(HashMap::new()));
 
   for stream in listener.incoming() {
     let stream = stream.unwrap();
 
     println!("New Connection from {}", stream.peer_addr().unwrap());
-
-    loop {
-      if handle_connection(&stream, &mut connection_states) {
-        connection_states.remove(&stream.peer_addr().unwrap());
-        break;
+    let connection_states_clone = connection_states.clone();
+    std::thread::spawn(move || {
+      loop {
+        let mut stream = stream.try_clone().unwrap();
+        let packet = lib::utils::read_packet(&mut stream);
+      
+        if packet::handlers::handle_packet(packet, &mut stream, &mut connection_states_clone.lock().unwrap()) {
+          connection_states_clone.lock().unwrap().remove(&stream.peer_addr().unwrap());
+          break;
+        }
       }
-    }
+    });
   }
-}
-
-fn handle_connection(stream: &TcpStream, connection_states: &mut HashMap<SocketAddr, ConnectionState>) -> bool {
-  let mut stream = stream.try_clone().unwrap();
-  let packet = lib::utils::read_packet(&mut stream);
-
-  return packet::handlers::handle_packet(packet, &mut stream, connection_states);
 }
