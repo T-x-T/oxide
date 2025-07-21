195c8d79586f3a81cb5cb0bb29f0f03b6e142c6b
T-x-T <37040316+T-x-T@users.noreply.github.com>
2025-07-21 22:04:49 +0200

    restrict panic command to console

--- server/src/command/panic.rs
+++ server/src/command/panic.rs
@@ -10,7 +10,20 @@ pub fn init(game: &mut Game) {
 	});
 }
 
-fn execute(_command: String, _stream: Option<&mut TcpStream>, _game: &mut Game, _connection_streams: &mut HashMap<SocketAddr, TcpStream>, _connections: &mut HashMap<SocketAddr, Connection>) -> Result<(), Box<dyn Error>> {
+fn execute(_command: String, stream: Option<&mut TcpStream>, _game: &mut Game, _connection_streams: &mut HashMap<SocketAddr, TcpStream>, _connections: &mut HashMap<SocketAddr, Connection>) -> Result<(), Box<dyn Error>> {
+	if stream.is_some() {
+		lib::utils::send_packet(stream.unwrap(), lib::packets::clientbound::play::SystemChatMessage::PACKET_ID, lib::packets::clientbound::play::SystemChatMessage {
+			  content: NbtTag::TagCompound(None, vec![
+				NbtTag::String(Some("type".to_string()), "text".to_string()),
+				NbtTag::String(Some("text".to_string()), "this command is only intended to be used from the console".to_string()),
+			]),
+		  overlay: false,
+	 	}.try_into()?)?;
+
+		return Ok(());
+	}
+
+
 	eprintln!("panic command was used");
 	process::exit(1000);
 }
