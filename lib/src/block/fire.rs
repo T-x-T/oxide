f404fc64671e9e8b9812bd3682ef8f7876325e6f
TheTxT <contact@thetxt.io>
2026-05-11 21:22:29 +0200

    make nether_portal placement logic work with differnt sizes and orientations of portals #78

--- lib/src/block/fire.rs
+++ lib/src/block/fire.rs
@@ -9,7 +9,6 @@ pub fn get_block_state_id(
 	let mut output: Vec<(u16, BlockPosition)> = Vec::new();
 
 	let obsidian_block_state_id = data::blocks::get_block_from_name("minecraft:obsidian", block_states).states.first().unwrap().id;
-	let nether_portal_block_state_id = data::blocks::get_block_from_name("minecraft:nether_portal", block_states).states.first().unwrap().id;
 
 	let block_state_id_below = dimension
 		.get_block(BlockPosition {
@@ -18,97 +17,125 @@ pub fn get_block_state_id(
 		})
 		.unwrap_or_default();
 
+	let mut valid = true;
 	if obsidian_block_state_id == block_state_id_below {
-		let next_blocks_to_check = vec![
-			BlockPosition {
-				x: position.x + 1,
-				y: position.y - 1,
-				..position
-			},
-			BlockPosition {
-				x: position.x + 2,
-				..position
-			},
-			BlockPosition {
-				x: position.x + 2,
-				y: position.y + 1,
-				..position
-			},
-			BlockPosition {
-				x: position.x + 2,
-				y: position.y + 2,
-				..position
-			},
-			BlockPosition {
-				x: position.x + 1,
-				y: position.y + 3,
-				..position
-			},
-			BlockPosition {
-				y: position.y + 3,
-				..position
-			},
-			BlockPosition {
-				x: position.x - 1,
-				y: position.y + 2,
-				..position
-			},
-			BlockPosition {
-				x: position.x - 1,
-				..position
-			},
-		];
-
-		for next_block_to_check in next_blocks_to_check {
-			let block_state_id = dimension.get_block(next_block_to_check).unwrap_or_default();
-			if block_state_id != obsidian_block_state_id {
-				let fire_block = data::blocks::get_block_from_name("minecraft:fire", block_states);
-				let fire_block_state_id = fire_block.states[fire_block.default_state].id;
-				return vec![(fire_block_state_id, position)];
+		let portal_blocks = get_portal_block_positions(dimension, position);
+		for position_to_check in &portal_blocks {
+			if !dimension.get_block(*position_to_check).is_ok_and(|x| x == 0) {
+				valid = false;
+				break;
 			}
 		}
-		output = vec![
-			(nether_portal_block_state_id, position),
-			(
-				nether_portal_block_state_id,
-				BlockPosition {
-					x: position.x + 1,
-					..position
-				},
-			),
-			(
-				nether_portal_block_state_id,
-				BlockPosition {
-					y: position.y + 1,
-					..position
-				},
-			),
-			(
-				nether_portal_block_state_id,
-				BlockPosition {
-					y: position.y + 1,
-					x: position.x + 1,
-					..position
-				},
-			),
-			(
-				nether_portal_block_state_id,
-				BlockPosition {
-					x: position.x + 1,
-					y: position.y + 2,
-					..position
-				},
-			),
-			(
-				nether_portal_block_state_id,
-				BlockPosition {
-					y: position.y + 2,
-					..position
-				},
-			),
-		]
+		let first_x = portal_blocks.first().unwrap().x;
+		let last_x = portal_blocks.last().unwrap().x;
+
+		let nether_portal_block_state_id = if first_x != last_x {
+			data::blocks::get_block_from_name("minecraft:nether_portal", block_states).states.first().unwrap().id
+		} else {
+			data::blocks::get_block_from_name("minecraft:nether_portal", block_states).states.last().unwrap().id
+		};
+		for portal_block in &portal_blocks {
+			output.push((nether_portal_block_state_id, *portal_block));
+		}
 	}
 
+	if !valid {
+		let fire_block = data::blocks::get_block_from_name("minecraft:fire", block_states);
+		let fire_block_state_id = fire_block.states[fire_block.default_state].id;
+		return vec![(fire_block_state_id, position)];
+	}
 
 	return output;
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Direction {
+	XPos,
+	XNeg,
+	YPos,
+	YNeg,
+	ZPos,
+	ZNeg,
+}
+
+fn get_portal_block_positions(dimension: &Dimension, starting_position: BlockPosition) -> Vec<BlockPosition> {
+	let mut portal_blocks: Vec<BlockPosition> = Vec::new();
+
+	let y_pos_blocks = find_air_blocks_in_direction(dimension, Direction::YPos, starting_position);
+	let y_neg_blocks = find_air_blocks_in_direction(dimension, Direction::YNeg, starting_position);
+
+	if ((y_pos_blocks + y_neg_blocks) >= 2) && ((y_pos_blocks + y_neg_blocks) <= 23) {
+		let x_pos_blocks = find_air_blocks_in_direction(dimension, Direction::XPos, starting_position);
+		let x_neg_blocks = find_air_blocks_in_direction(dimension, Direction::XNeg, starting_position);
+
+		if ((x_pos_blocks + x_neg_blocks) >= 1) && ((x_pos_blocks + x_neg_blocks) <= 23) {
+			for x in -x_neg_blocks..=x_pos_blocks {
+				for y in -y_neg_blocks..=y_pos_blocks {
+					portal_blocks.push(BlockPosition {
+						x: starting_position.x + x as i32,
+						y: starting_position.y + y as i16,
+						z: starting_position.z,
+					});
+				}
+			}
+		} else {
+			let z_pos_blocks = find_air_blocks_in_direction(dimension, Direction::ZPos, starting_position);
+			let z_neg_blocks = find_air_blocks_in_direction(dimension, Direction::ZNeg, starting_position);
+
+			if ((z_pos_blocks + z_neg_blocks) >= 1) && ((z_pos_blocks + z_neg_blocks) <= 23) {
+				for z in -z_neg_blocks..=z_pos_blocks {
+					for y in -y_neg_blocks..=y_pos_blocks {
+						portal_blocks.push(BlockPosition {
+							x: starting_position.x,
+							y: starting_position.y + y as i16,
+							z: starting_position.z + z as i32,
+						});
+					}
+				}
+			}
+		}
+	}
+
+	return portal_blocks;
+}
+
+fn find_air_blocks_in_direction(dimension: &Dimension, direction: Direction, starting_position: BlockPosition) -> isize {
+	let mut num_of_blocks = 0;
+
+	for i in 1..=23 {
+		let position_to_check = match direction {
+			Direction::XPos => BlockPosition {
+				x: starting_position.x + i,
+				..starting_position
+			},
+			Direction::XNeg => BlockPosition {
+				x: starting_position.x - i,
+				..starting_position
+			},
+			Direction::YPos => BlockPosition {
+				y: starting_position.y + i as i16,
+				..starting_position
+			},
+			Direction::YNeg => BlockPosition {
+				y: starting_position.y - i as i16,
+				..starting_position
+			},
+			Direction::ZPos => BlockPosition {
+				z: starting_position.z + i,
+				..starting_position
+			},
+			Direction::ZNeg => BlockPosition {
+				z: starting_position.z - i,
+				..starting_position
+			},
+		};
+
+		if dimension.get_block(position_to_check).is_ok_and(|x| x == 0) {
+			num_of_blocks += 1;
+		} else {
+			return num_of_blocks;
+		}
+	}
+
+	return num_of_blocks;
 }
